2f9b3c2b1693368b7b6bdb9bd3cf0da955819495
ohadk <ohadkachlon@gmail.com>
2023-01-08 11:38:00 +0200

    ex1.1

---
+++ ex1-1.js
@@ -0,0 +1,33 @@
+//string
+let string="dog";
+console.log(string);
+//number
+let num= 1332;
+console.log(num);
+//object
+const object= {
+    engine: { cylinders: 4, size: 2.2 },
+  };
+  console.log(object.engine.cylinders);  
+//boolean
+const Booleano = (10 > 9);
+console.log(Booleano);
+//null
+let numbers = null;
+console.log(numbers);
+//undefined
+const x = {
+    engine: { cylinders: 4, size: 2.2 },
+  };
+  console.log(x.engine.cylinder); 
+
+  const sym1 = Symbol("number1");
+  console.log(sym1);
+
+
+
+
+
+
+
+  
