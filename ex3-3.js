86726d592fd817b3819b67cc9bd2b89890f94091
ohadk <ohadkachlon@gmail.com>
2023-01-09 15:43:09 +0200

    3.3

---
+++ ex3-3.js
@@ -0,0 +1,18 @@
+function percentageOfWorld1(population) {
+    let pop = (population / 7900) * 100;
+    return pop.toFixed(2);
+  }  
+  const  jordan = percentageOfWorld1(10000);
+  const  israel = percentageOfWorld1(9000);
+  const  egypt  = percentageOfWorld1(12000);
+ 
+  
+
+  const percentageOfWorld2 = function (population) {
+    let pop = (population / 7900) * 100;
+    return pop.toFixed(2);
+  };
+  
+  const spain = percentageOfWorld2(10000);  
+  const france = percentageOfWorld2(10000);
+  const usa = percentageOfWorld2(10000);
---
+++ numberornull.js
@@ -0,0 +1,4 @@
+num = prompt('number or null ??');
+if (num == null || num == '' || isNaN(num) <= 0);
+num = parseInt(num);
+  console.log(num);  
