1b1f5605468dcd5cea99dd0480df363838e9aeb7
ohadk <ohadkachlon@gmail.com>
2023-01-09 22:57:13 +0200

    ex-4.2

---
+++ ex4.2.js
@@ -0,0 +1,15 @@
+const gradeAssigner = (score) => {
+    if (score <= 64) {
+        return "F";
+    } else if (score >= 65 && score < 70 ) {
+        return "D";
+    } else if (score >= 69 && score < 80 ) {
+        return "C";
+    } else if (score >= 79 && score < 90 ) {
+        return "B";
+    } else if (score >= 89 && score <= 100 ) {
+        return "A";
+    }
+}
+
+console.log(`your score is ${gradeAssigner(100)}`);
