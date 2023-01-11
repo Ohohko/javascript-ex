9e8c51162dc9815a3325e9e24d3c599fc063142b
ohadk <ohadkachlon@gmail.com>
2023-01-11 21:36:49 +0200

    8-4

--- ex8-4.js
+++ ex8-4.js
@@ -1,5 +1,21 @@
 function countDuplicat(arr) {
     let objDuplicate = {};
     const str = arr.join();
-    const arr1 = str.split(" ")
-}
+    const arr1 = str.split(" ");
+    let letter = [];
+    let count = 1;
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] === arr1[i + 1]) {
+        count++;
+      } else {
+        let value = `${count}${arr1[i]}`;
+        letter = [...letter, value];
+        count = 1;
+      }
+    }
+    return (objDuplicate = Object.assign({}, letter));
+  }
+  
+  console.log(
+    countDuplicat(["Hello", "Good Day", "Your Welcome", "hotdog", "hamburgers"])
+  );
