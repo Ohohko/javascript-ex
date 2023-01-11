bb8a9798c0835711922d542a0867299992ed07f4
ohadk <ohadkachlon@gmail.com>
2023-01-11 16:44:17 +0200

    8-2

--- ex8-2.js
+++ ex8-2.js
@@ -4,4 +4,13 @@ const myCountry= {
     language:"En",
     population: 350000000,
     neighbors: ["Mexico"],
-}
+
+    describe: function () {
+      return `${this.country} has ${this.population} million people, their mother tongue is ${this.language}, they have ${this.neighbors.length} countries and a capital called ${this.capital}`;
+    }, 
+    checkIsland: function () {
+        this.neighbors.length > 0 ? (this.isIsland = false) : (this.isIsland = true);
+        return this.isIsland;
+      },
+    };
+  console.log(myCountry);
