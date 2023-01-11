8a0a1395facf749c81fda79409d5c90f6d423408
ohadk <ohadkachlon@gmail.com>
2023-01-11 21:45:15 +0200

    8-5

--- ex8-5.js
+++ ex8-5.js
@@ -6,3 +6,14 @@ const obj1 = {
     firstName: "John",
     lastName: "Doe",
   };
+  function swap(obj) {
+    let result = {
+    };
+    for (let i in obj) {
+      result[obj[i]] = i;
+      console.log(obj[i]);
+      console.log(i);
+    }
+    return result;
+  }
+  console.log(swap(obj1));
