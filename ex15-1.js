bbaa82b0cc7aa2e5778288ce3c468e90f660df48
ohadk <ohadkachlon@gmail.com>
2023-01-18 13:21:35 +0200

    ex15.1

---
+++ ex15-1.js
@@ -0,0 +1,10 @@
+function getSum(arr1, arr2){
+const sum = 0;
+for (let i=0; i < getSum[arr1]; i++){
+sum += arr1[i];
+}
+for (let i=0; i < getSum[arr2]; i++){
+sum += arr2[i];
+}
+}
+getSum([1,2,3][5,66,23]);
